79ffa91ae0c0cc05c0db0f9eac85ae5277f0438a
lijunpei <ljp14290@cnki.net>
2021-07-14 16:44:48 +0800

    打包为 jar

--- src/main/java/com/jasper/qrcode/QrCodeApplication.java
+++ src/main/java/com/jasper/qrcode/QrCodeApplication.java
@@ -2,18 +2,11 @@ package com.jasper.qrcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class QrCodeApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(QrCodeApplication.class);
-    }
+public class QrCodeApplication {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(QrCodeApplication.class, args);
